3ae9b75b419de5dfce7b4020a832950e84bbf824
Rafael José <jtl77rafael@gmail.com>
2025-04-11 16:02:23 -0300

    chore: add base structure

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const GENESI_RPG = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+GENESI_RPG.abilities = {
+  str: 'GENESI_RPG.Ability.Str.long',
+  dex: 'GENESI_RPG.Ability.Dex.long',
+  con: 'GENESI_RPG.Ability.Con.long',
+  int: 'GENESI_RPG.Ability.Int.long',
+  wis: 'GENESI_RPG.Ability.Wis.long',
+  cha: 'GENESI_RPG.Ability.Cha.long',
+};
+
+GENESI_RPG.abilityAbbreviations = {
+  str: 'GENESI_RPG.Ability.Str.abbr',
+  dex: 'GENESI_RPG.Ability.Dex.abbr',
+  con: 'GENESI_RPG.Ability.Con.abbr',
+  int: 'GENESI_RPG.Ability.Int.abbr',
+  wis: 'GENESI_RPG.Ability.Wis.abbr',
+  cha: 'GENESI_RPG.Ability.Cha.abbr',
+};
